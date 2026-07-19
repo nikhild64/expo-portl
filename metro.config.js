fd914185eb25b3830963fe099e0ed35cf42eea9e
Nikhil Dhawan <nikhild64@gmail.com>
2026-07-19 21:31:52 +0530

    feat: add asset directory for documentation and configure metro to exclude media from bundles

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,30 @@
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+const path = require('path');
 const { withUniwindConfig } = require('uniwind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 let config = getSentryExpoConfig(__dirname);
+
+function escapeRegex(value) {
+  return value.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+}
+
+const screenshotsVideoPath = path.resolve(__dirname, 'assets/screenshots-video');
+const existingBlockList = config.resolver?.blockList;
+const blockListPatterns = Array.isArray(existingBlockList)
+  ? existingBlockList
+  : existingBlockList
+    ? [existingBlockList]
+    : [];
+
+config.resolver = {
+  ...config.resolver,
+  // Keep submission screenshots/video in git, but out of Metro and APK bundles.
+  blockList: [
+    ...blockListPatterns,
+    new RegExp(`${escapeRegex(screenshotsVideoPath)}[/\\\\].*`),
+  ],
+};
 
 config = withUniwindConfig(config, {
   cssEntryFile: './src/global.css',
